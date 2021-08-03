f4c8724e2ea65a8f4fa7ccf1818360572a2d3e01
usman-intelliscence <usman@intelliscence.com>
2021-08-03 18:10:02 +0500

    code

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ app.get('/',(req,res)=>{
     res.send('API is runningn')
 })
 
+app.use('/api/users', require('./Routes/Api/users'))
+app.use('/api/auth', require('./Routes/Api/auth'))
+app.use('/api/profile', require('./Routes/Api/profile'))
+app.use('/api/post', require('./Routes/Api/post'))
+
 app.listen(PORT,()=>{
     console.log(`Server is working on ${PORT}`)
 })
