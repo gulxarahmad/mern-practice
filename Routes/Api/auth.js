e0ec9ea104024417fd2d7a48b70d7d3340a9b372
usman-intelliscence <usman@intelliscence.com>
2021-08-05 17:38:05 +0500

    code

--- Routes/Api/auth.js
+++ Routes/Api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const auth = require('../../Middleware/auth')
 const Users = require('../../Models/User')
+const {check, validationResult} = require('express-validator/check')
+const bkrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
 
 const router = express.Router();
 
@@ -15,6 +19,54 @@ router.get('/',auth,async(req,res)=>{
     catch(err){
         console.log(err.message)
         return res.status(400).send('Server problem')
+    }
+})
+
+router.post('/',[
+
+    check('email','Please Enter a Valid Email')
+    .isEmail(),
+    check('password','Please Enter Password of 6 or more digits')
+    .exists()
+], async(req,res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+
+    const { email, password} = req.body
+
+    try{
+        let user = await Users.findOne({email})
+        if(!user){
+            return res.status(400).json({errors:[{msg:'Email Does Not Exist'}]})
+        }
+
+        const isMatch = await bkrypt.compare(password, user.password)
+        if(!isMatch){
+          return res.status(400).json({errors:[{msg:'Your Password is Wrong'}]})
+        }
+
+        const payload = {
+            user:{
+                id:user.id
+            }
+        }
+
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn:3600000},
+            (err,token)=>{
+            if(err) throw err;
+            res.json({token});
+            }
+        )
+       // return res.status(200).send('Hurray you are registered')
+    }
+    catch(err){
+        console.log(err.message);
+        return res.status(500).send('Server is causing problem')
     }
 })
 
