176ab7a3a7ab991fd576b30399c3f8d87d20df07
usman-intelliscence <usman@intelliscence.com>
2021-08-06 12:06:07 +0500

    code

--- Routes/Api/profile.js
+++ Routes/Api/profile.js
@@ -8,16 +8,19 @@ const router = express.Router();
 
 router.get('/me',auth,async(req,res)=>{
     try{
-        const profile = await userProfile.findOne({user:req.user.id}).populate('users',
+        console.log(req.user.id)
+        const profile = await userProfile.findOne({user:req.user.id}).populate('user',
         ['name','avatar'])
         if(!profile){
             return res.status(500).json({msg:'There is no profile exist'})
         }
-        res.json(profile)
+        return res.json(profile)
     }
     catch(err){
+
         console.log(err.message);
-        res.status(500).send('Server Problem')
+
+        return res.status(500).send('Server Problem')
     }
     
 })
@@ -69,6 +72,7 @@ router.post('/create',[auth,[
     if(instagram)  profileFields.social.instagram = instagram
 
     try{
+        
         let profile = await userProfile.findOne({user:req.user.id});
         if(profile){
             console.log('update part running')
@@ -100,6 +104,81 @@ router.get('/all-profiles',async(req,res)=>{
     }
     catch(err){
         return res.send(err.message)
+    }
+})
+
+router.get('/user/:user_id',async(req,res)=>{
+    try{
+        const profile = await userProfile.findOne({user:req.params.user_id}).populate('user',['name','avatar'])
+        if(!profile){
+            return res.status(400).send({msg:'Profile does not exist'})
+        }
+        return res.json(profile)
+    }
+    catch(err){
+     if(err.kind == 'ObjectId')
+        {
+            return res.status(500).json({msg:'There is no profile exist'})
+        }
+        return res.send(err.message)
+    }
+})
+
+router.delete('/',auth, async(req,res)=>{
+    try{
+
+        await userProfile.findOneAndDelete({user:req.user.id})
+        await Users.findOneAndDelete({_id:req.user.id})
+        return res.status(400).json({msg:'User has been deleted'})
+
+    }
+    catch(err){
+        return res.send('Sever Error')
+    }
+})
+
+router.put('/add_experience',[auth,[
+    check('title','Please Add Title').not().isEmpty(),
+    check('company','Please Add company').not().isEmpty(),
+    check('from','Please Add Date of Joining').not().isEmpty()
+]],async(req,res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body
+
+    const newExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try{
+        console.log(req.body.id)
+        const profile = await userProfile.findOne({user:req.user.id})
+
+        
+        profile.experience.unshift(newExp)
+        await profile.save();
+        return res.status(400).json(profile)
+    }
+    catch(err){
+        console.log(err.message)
+        return res.status(400).json({msg:'Server Issue'})
     }
 })
 
